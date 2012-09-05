d695b0ffedb3dc2c65108404a3c9239ea79d56a5
BEER <Apple@Apples-MacBook-Air.local>
2012-09-05 11:05:59 +0700

    Initial Commit

---
+++ otw/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  otw
+//
+//  Created by BEER on 9/5/55 BE.
+//  Copyright (c) 2555 BEER. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+

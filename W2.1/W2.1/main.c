77d164660089949c9228802b5e5ce4eddea2ed2f
warrunyou1 <warrunyou1234>
2012-09-13 10:43:06 +0700

    pLuGUp

---
+++ W2.1/W2.1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  W2.1
+//
+//  Created by MasterDragon on 9/6/55 BE.
+//  Copyright (c) 2555 MasterDragon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int n[3][5] = {0};
+    int i,j;
+    printf("EDIT NOW");
+    // qarayu
+}
+
